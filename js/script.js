1aa0ac0309fb1700fd74e27aee9c40985f32d1e3
Wilka Gmurczyk <xwilmax@o2.pl>
2020-02-23 22:56:44 +0100

    Add new positions for function generateTags

--- js/script.js
+++ js/script.js
@@ -51,7 +51,9 @@
     optTitleListSelector = '.titles',
     optArticleTagsSelector = '.post-tags .list',
     optTagsListSelector = '.list .tags',
-    optArticleAuthorSelector = '.post-author';
+    optArticleAuthorSelector = '.post-author',
+    optAuthorsListSelector = '.authors';
+
 
 
   function generateTitleLinks(customSelector = '') {
@@ -89,6 +91,9 @@
   generateTitleLinks();
 
   function generateTags() {
+    /* [NEW] create a new variable allTags with an empty array */
+    let allTags = [];
+
 
     /* find all articles */
     const articles = document.querySelectorAll(optArticleSelector);
@@ -120,13 +125,23 @@
 
         /* add generated code to html variable */
         html = html + linkHTML;
+        /* [NEW] check if this link is NOT already in allTags */
+       if(allTags.indexOf(linkHTML) == -1){
+         /* [NEW] add generated code to allTags array */
+         allTags.push(linkHTML);
+       }
 
       /* END LOOP: for each tag */
       }
-      /* insert HTML of all the links into the tags wrapper */
-      titleList.innerHTML = html;
+    /* insert HTML of all the links into the tags wrapper */
+    titleList.innerHTML = html;
 
-      /* END LOOP: for every article: */
+    /* END LOOP: for every article: */
+    /* [NEW] find list of tags in right column */
+    const tagList = document.querySelector('.tags');
+
+    /* [NEW] add html from allTags to tagList */
+    tagList.innerHTML = allTags.join(' ');
 
     }
   }
@@ -246,7 +261,4 @@ addClickListenersToTags();
     }
   }
    addClickListenersToAuthors();
-// function authorClickHandler
-
-
   }
