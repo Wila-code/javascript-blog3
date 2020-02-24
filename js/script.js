128e8b0b8c89132ce16dbb0d54b02bd5fdca7679
Wilka Gmurczyk <xwilmax@o2.pl>
2020-02-24 21:02:41 +0100

    Fix function tagClickHandler

--- js/script.js
+++ js/script.js
@@ -151,7 +151,7 @@
   const clickedElement = this;
 
   /* make a new constant "href" and read the attribute "href" of the clicked element */
-  const href = clickedElement.getAttribute(href);
+  const href = clickedElement.getAttribute('href');
 
   /* make a new constant "tag" and extract tag from the "href" constant */
   const tag = href.replace('#tag-', '');
@@ -166,7 +166,7 @@
     /* END LOOP: for each active tag link */
   }
   /* find all tag links with "href" attribute equal to the "href" constant */
-  const hTag = document.querySelectorAll('a[href="'+ href + '"]');
+  const hTags = document.querySelectorAll('a[href="'+ href + '"]');
 
   /* START LOOP: for each found tag link */
   for(let hTag of hTags){
@@ -214,14 +214,14 @@ addClickListenersToTags();
   }
   generateAuthors();
 
-  
+
   function authorClickHandler(event){
     /* prevent default action for this event */
     event.preventDefault();
     /* make new constant named "clickedElement" and give it the value of "this" */
     const clickedElement = this;
     /* make a new constant "href" and read the attribute "href" of the clicked element */
-    const href = clickedElement.getAttribute(href);
+    const href = clickedElement.getAttribute('href');
     /* make a new constant "author" and extract author from the "href" constant */
     const author = href.replace('#author-', '');
     /* find all author links with class active */
